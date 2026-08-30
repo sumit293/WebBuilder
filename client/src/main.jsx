0de74741700016c9172bdcc3e3422e083ea2d1bf
sumi <sumitwani310@gmail.com>
2026-08-31 00:47:27 +0530

    userslice, store ,reducer methods initilzed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
+import { store } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <provider>
+  <provider store={store}>
     <App />
+  
   </provider>
  
 )
